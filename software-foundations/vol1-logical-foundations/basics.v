93e660b0d9541180e692d66f4670fe7fa052c8e3
sofia <sofrose@proton.me>
2025-03-10 02:09:11 -0400

    start logical foundations basics

--- software-foundations/vol1-logical-foundations/basics.v
+++ software-foundations/vol1-logical-foundations/basics.v
@@ -7,7 +7,7 @@ Inductive day : Type :=
   | saturday
   | sunday.
 
-Definition next_weekday (d:day) : day :=
+Definition next_working_day (d:day) : day :=
   match d with
   | monday => tuesday
   | tuesday => wednesday
@@ -18,11 +18,156 @@ Definition next_weekday (d:day) : day :=
   | sunday => monday
   end.
 
-Compute (next_weekday friday).
+Compute (next_working_day friday).
 
-Compute (next_weekday (next_weekday saturday)).
+Compute (next_working_day (next_working_day saturday)).
 
-Example test_next_weekday :
-  (next_weekday (next_weekday saturday)) = tuesday.
+Example test_next_working_day : (next_working_day (next_working_day saturday)) = tuesday.
+Proof.
+  simpl.
+  reflexivity.
+Qed.
 
+From Coq Require Export String.
+
+Inductive bool : Type :=
+  | true
+  | false.
+
+Definition negb (b:bool) : bool :=
+  match b with
+  | true => false
+  | false => true
+  end.
+
+Definition andb (b1 : bool) (b2 : bool) : bool :=
+  match b1 with
+  | true => b2
+  | false => false
+  end.
+
+Definition orb (b1 : bool) (b2 : bool) : bool :=
+  match b1 with
+  | true => true
+  | false => b2
+  end.
+
+Example test_orb1: (orb true false) = true.
 Proof. simpl. reflexivity. Qed.
+
+Example test_orb2: (orb false false) = false.
+Proof. simpl. reflexivity. Qed.
+
+Example test_orb3: (orb false true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_orb4: (orb true true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Notation "x && y" := (andb x y).
+Notation "x || y" := (orb x y).
+
+Example test_orb5: false || false || true = true.
+Proof. simpl. reflexivity. Qed.
+
+Definition negb' (b : bool) : bool :=
+  if b then false else true.
+
+Definition andb' (b1 : bool) (b2 : bool) : bool :=
+  if b1 then b2 else false.
+
+Definition orb' (b1 : bool) (b2: bool) : bool :=
+  if b1 then true else b2.
+
+Inductive bw: Type :=
+  | bw_black
+  | bw_white.
+
+Definition invert (x : bw) : bw :=
+  if x then bw_white else bw_black.
+
+Compute (invert bw_black).
+
+Compute (invert bw_white).
+
+Definition nandb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+  | false => true
+  | true => negb b2
+  end.
+
+Example test_nandb1: (nandb true false) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb2: (nandb false false) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb3: (nandb false true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb4: (nandb true true) = false.
+Proof. simpl. reflexivity. Qed.
+
+Definition andb3 (b1:bool) (b2:bool) (b3:bool) : bool :=
+  match b1 with
+  | false => false
+  | true =>
+      match b2 with
+      | false => false
+      | true => b3
+      end
+  end.
+
+Example test_andb31: (andb3 true true true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_andb32: (andb3 false true true) = false.
+Proof. simpl. reflexivity. Qed.
+
+Example test_andb33: (andb3 true false true) = false.
+Proof. simpl. reflexivity. Qed.
+
+Example test_andb34: (andb3 true true false) = false.
+Proof. simpl. reflexivity. Qed.
+
+Check true.
+
+Check true : bool.
+
+Check (negb true) : bool.
+
+Check negb : bool -> bool.
+
+Inductive rgb : Type :=
+  | red
+  | green
+  | blue.
+
+Inductive color : Type :=
+  | black
+  | white
+  | primary (p : rgb).
+
+Definition monochrome (c : color) : bool :=
+  match c with
+  | black => true
+  | white => true
+  | primary p => false
+  end.
+
+Definition isread (c : color) : bool :=
+  match c with
+  | black => false
+  | white => false
+  | primary red => true
+  | primary _ => false
+  end.
+
+Module Playground.
+  Definition foo : rgb := blue.
+End Playground.
+
+Definition foo : bool := true.
+
+Check Playground.foo : rgb.
+Check foo : bool.
